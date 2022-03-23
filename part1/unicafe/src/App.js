0ca30eb8754021e570cf259bc4a7ba0fbca7ffa2
Siccen2 <martin.harmala@centria.fi>
2022-03-23 15:07:49 +0200

    part1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <h3>No feedback given</h3>
+      </>
+    )
+  }
   return (
     <>
       <h1>Statistics</h1>
@@ -46,7 +54,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all} average = {average}  />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} />
 
 
 
