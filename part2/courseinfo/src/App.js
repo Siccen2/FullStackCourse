4fc5c0fb009eacce175a5c02cd518c9c4546b41e
Martin Härmälä <martin.harmala@centria.fi>
2022-03-30 20:20:47 +0300

    part2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,42 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ exercises }) => {
-  const sum = exercises.reduce((total, part) => total + part)
-  return (
-    <div>
-      <p> <b> Total of {sum} exercises</b> </p>
-    </div>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  console.log(name, exercises)
-  return(
-    <p>
-    {name} {exercises}
-    </p>
-  )
-}
- 
-
-const Content = ({ parts }) =>
-  <div>
-    {parts.map(part =>
-      <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-  </div>
-
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts.map(part => part.exercises)} />
-
-    </div>
-  )
-
-}
+import Course from "./components/Course"
 
 const App = () => {
 
