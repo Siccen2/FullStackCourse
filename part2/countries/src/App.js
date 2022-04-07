8be6476c1a11001abe1cf074744bf69b063ea8e9
Siccen2 <martin.harmala@centria.fi>
2022-04-07 12:23:04 +0300

    part2.12 almost

---
+++ part2/countries/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const NationFinder = ({ searchWord, nationstoshow }) => {
+  if (nationstoshow.length === 1) {
+    return (
+      <div>
+        <h1>{nationstoshow[0].name.common}</h1>
+        <div>
+          capital {nationstoshow[0].capital.toString()}
+        </div>
+        <div>
+          population {nationstoshow[0].population.toString()}
+        </div>
+        <div>
+          <h3>languages</h3>
+          <ul>
+            {Object.values(nationstoshow[0].languages).map(language => <li key={language}>{language}</li>)}
+          </ul>
+          <div>
+            <img src={nationstoshow[0].flags.png} alt={nationstoshow[0].name.common} height="100" width="100" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  else {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  }
+
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchWord, setSearchWord] = useState('')
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('help', response.data)
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  const nationstoshow = countries.filter(country => country.name.common.toLowerCase().includes(searchWord.toLowerCase().trim()))
+  console.log('nationshow', nationstoshow)
+
+  const handleSearchChange = (event) => {
+    setSearchWord(event.target.value)
+  }
+  return (
+    <div>
+      find countries <input value={searchWord} onChange={handleSearchChange} />
+      <div>
+      </div>
+      <NationFinder searchWord={searchWord} nationstoshow={nationstoshow} />
+    </div >
+
+
+  );
+}
+
+export default App;
