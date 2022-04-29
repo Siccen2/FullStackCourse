30611c8e39da73d96a8894e844f47fe073b6875e
Siccen2 <martin.harmala@centria.fi>
2022-04-29 13:19:01 +0300

    part4.4

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+  return 1
+}
+const totalLikes = (blogs) => {
+  let sum=0
+  for(i=0;i<blogs.length;i++)
+  {
+    sum+=blogs[i].likes
+  }
+  return sum
+}
+
+module.exports = {
+  dummy, totalLikes
+}
